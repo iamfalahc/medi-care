c49cd775c7f1a4f1d8d92736ae0b8b2d43e89549
Muhammed Falah <iamfalahc@gmail.com>
2024-05-26 23:38:57 +0530

    doctors login and signup form changed

--- front-end/src/doctor/sign-up/DoctorSignUp.jsx
+++ front-end/src/doctor/sign-up/DoctorSignUp.jsx
@@ -1,173 +1,39 @@
-import * as React from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
+import React from "react";
 import { Link } from "react-router-dom";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import "../doctor.css"
 
-function DoctorSignUp() {
-  const defaultTheme = createTheme();
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      name: data.get("name"),
-      specialty: data.get("specialty"),
-      qualification: data.get("qualification"),
-      experience: data.get("experience"),
-      fees: data.get("fees"),
-      email: data.get("email"),
-      password: data.get("password"),
-      image: data.get("image"),
-    });
-  };
+const DoctorSignUp = () => {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <CssBaseline />
+    <div className="doctor-signup">
+      <div className="form-container">
+      <span className="title">Join as Doctor</span>
+      <div className="field-group">
+        <input type="text" placeholder="Name . . ." />
+        <input type="text" placeholder="Email . . ."/>
+      </div>
+      <div className="field-group">
+        <input type="text" placeholder="Qualification"/>
+        <input type="text" placeholder="Specialty . . ."/>
+      </div>
+      <div className="field-group">
+        <input type="text" placeholder="Experience . . ."/>
+        <input type="text" placeholder="Fees . . ."/>
+      </div>
+      <div className="field-group">
+        <input type="text" placeholder="Image . . ."/>
+        <input type="text" placeholder="Password"/>
+      </div>
+     
+      <input className="cpassword-input" type="text" placeholder="Confirm password. . ." />
+  
 
-      <Container component="main" maxWidth="xs">
-        <Box
-          sx={{
-            marginTop: 0,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Join as a Doctor
-          </Typography>
-          <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 3 }}
-          >
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="name"
-                  required
-                  fullWidth
-                  id="name"
-                  label="Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="specialty"
-                  label="Specialty"
-                  name="specialty"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="qualification"
-                  required
-                  fullWidth
-                  id="qualification"
-                  label="Qualification"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="experience"
-                  label="Experience"
-                  name="experience"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <div
-                class="mb-3 mt-3"
-                style={{ width: "100%", paddingLeft: "4%" }}
-              >
-                <input
-                  class="form-control"
-                  type="file"
-                  id="formFile"
-                  name="image"
-                  style={{ padding: "3%" }}
-                />
-              </div>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="fees"
-                  required
-                  fullWidth
-                  id="fees"
-                  label="Fees"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                />
-              </Grid>
-
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-            </Grid>
-            <Link to={"/doctor-home"}>
-              {" "}
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Join
-              </Button>
-            </Link>
-            <Grid container justifyContent="center">
-              <Grid item>
-                <Link to={"/doctor-login"} variant="body2">
-                  <span style={{ color: "blue" }}>
-                    Already have an account? Log in
-                  </span>
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
+      <div className="bottom-container">
+      <button>JOIN</button>
+      <span>Already have an account ? <Link to={"/doctor-login"}>Login</Link></span>
+      </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default DoctorSignUp;
